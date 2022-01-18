ad1a828db1891ccc020d98e32357e01f7b1bb3a2
MauricioMC97 <36861255+MauricioMC97@users.noreply.github.com>
2022-01-17 22:12:08 -0500

    Add files via upload

---
+++ BegoSys.Service/Controllers/AccountingController.cs
@@ -0,0 +1,53 @@
+﻿using BegoSys.Common.Constantes;
+using BegoSys.Core.Contabilidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using System.Web.Mvc;
+
+namespace BegoSys.Service.Controllers
+{
+    public class AccountingController : ApiController
+    {
+        private IAccountingRepository _repository;
+
+        /// <summary>
+        /// Crea una nueva instancia del tipo <see cref="BegoSys.Service.Controllers.OperationController"/>
+        /// </summary>
+        /// <param name="repository">Instancia del repositorio inyectada a través de spring.</param>
+        public AccountingController(IAccountingRepository repository)
+        {
+            _repository = repository;
+        }
+
+        ///<summary>
+        ///Registra las transacciones contables en el libro mayor
+        ///</summary>
+        /// <summary>
+        /// Obtiene la información del local y de las personas que trabajan en el.
+        /// </summary>
+        /// <param name="idLocal"></param>
+        /// <returns></returns>
+        //[HttpPost]
+        //[ResponseType(typeof(DatosLibroMayorTO))]
+        //[Route(ConstantesApi.GuardarenLibroMayor)]
+        //[EnableCors(origins: "*", headers: "*", methods: "*")]
+        //public IHttpActionResult GuardarLibroMayor(DatosLibroMayorTO DatosLM)
+        //{
+        //    bool RegistroOK = _repository.RegistrarLibroMayor(DatosLM);
+
+        //    if (RegistroOK)
+        //    {
+        //        return this.Ok(infoLocal);
+        //    }
+        //    else
+        //    {
+        //        return this.NotFound();
+        //    }
+        //}
+    }
+}
---
+++ BegoSys.Service/Interceptors/MethodInterceptor.cs
@@ -0,0 +1,48 @@
+﻿using AopAlliance.Intercept;
+using BegoSys.Common.Atributos;
+using BegoSys.Common.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BegoSys.Service.Interceptors
+{
+    public class MethodInterceptor : IMethodInterceptor
+    {
+        public object Invoke(IMethodInvocation invocation)
+        {
+            object result = null;
+            object[] customAttributes = invocation.Method.GetCustomAttributes(typeof(BegoSysRetryAttribute), true);
+
+            if (customAttributes != null && customAttributes.Length > 0)
+            {
+                //Se obtiene el valor del atributo
+                var customAttribute = (BegoSysRetryAttribute)customAttributes[0];
+
+                //Se crea el template que ejecutara los reintentos
+                BegoSysRetryTemplate template = new BegoSysRetryTemplate();
+
+                TimeSpan delay;
+
+                if (!TimeSpan.TryParse(customAttribute.Delay, out delay))
+                    delay = new TimeSpan(0, 0, 30);
+
+                //Se establecen las propiedades para los reintentos
+                template.Delay = delay;
+                template.MaxRetry = Convert.ToInt32(customAttribute.MaxRetry);
+                //Se procede a ejectuar el método
+                result = template.ExecuteWithRetry(delegate ()
+                {
+                    return invocation.Proceed();
+                });
+            }
+            else
+            {
+                //Se ejecuta el método normalmente
+                return invocation.Proceed();
+            }
+            return result;
+        }
+    }
+}
