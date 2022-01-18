93e5bad023cfcc08e382550ce62063d636c00abd
MauricioMC97 <36861255+MauricioMC97@users.noreply.github.com>
2022-01-17 23:00:11 -0500

    Add files via upload

---
+++ BegoSys.Service.Tests/Controllers/BillingControllerTest.cs
@@ -0,0 +1,107 @@
+﻿using BegoSys.Core.Facturacion;
+using BegoSys.Service.Controllers;
+using BegoSys.TO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+
+namespace BegoSys.Service.Tests.Controllers
+{
+
+    public class Token
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public int expires_in { get; set; }
+    }
+
+
+    [TestClass]
+    public class BillingControllerTest
+    {
+        private BillingController controller = new BillingController(new BillingRepository());
+
+
+
+        [TestMethod]
+        public void IngresarFacturas()
+        {
+            List<FacturaTO> datos = new List<FacturaTO>();
+
+            var factura = new FacturaTO
+            {
+                IdRegistro = 9426,
+                IdPedidoDia = 1,
+                NroResolucionDian = "18762012046532",
+                NroFacturaDian = "7508362",
+                Fecha = DateTime.Now,
+                TipoDespacho = 1,
+                Impuesto = 360,
+                ValorTotal = 4500,
+                EstadoFactura = "PENDIENTE",
+                IdPersona = 3,
+                IdLocal = 1
+
+                /*IdRegistro: 1,
+                IdPedidoDia: 1,
+                NroResolucionDian: "X",
+                NroFacturaDian: "X",
+                Fecha: frmPedidos.FechaHora1.value, //d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds(),
+                TipoDespacho: frmPedidos.TipoDespacho1.value,
+                Impuesto: frmPedidos.Impoconsumo.value,
+                ValorTotal: frmPedidos.ValorTotal.value,
+                EstadoFactura: 'PENDIENTE',
+                IdPersona: 3,
+                IdLocal: 1,
+                DetallePedido: [{
+                IdRegistro: 1,
+                    IdRegFactura: 1,
+                    IdProducto: frmPedidos.Producto1.value,
+                    Cantidad: frmPedidos.Cantidad1.value,
+                    ValorUnitario: frmPedidos.ValorUnitario1.value,
+                    Subtotal: frmPedidos.Subtotal1.value,
+                    Observaciones: frmPedidos.observaciones1.value
+                }*/
+        };
+            datos.Add(factura);
+
+
+            //token         
+            var clientToken = new RestClient("http://192.168.1.243/BegoSys.Service");
+            //var clientToken = new RestClient("http://localhost:3445");
+            //var clientToken = new RestClient("http://localhost/JBCService");
+
+
+            var requestToken = new RestRequest("token", Method.POST);
+            requestToken.AddParameter("grant_type", "password");
+            requestToken.AddParameter("username", "apiuser_bego");
+            requestToken.AddParameter("password", "bego2019*");
+
+            // execute the request
+            IRestResponse responseToken = clientToken.Execute(requestToken);
+
+            Token obj = JsonConvert.DeserializeObject<Token>(responseToken.Content);
+
+            var client = new RestClient("http://192.168.1.243/BegoSys.Service/api/Billing");
+
+            var request = new RestRequest("GuardarPedido", Method.POST);
+
+            request.AddJsonBody(datos);
+
+            request.AddHeader("Authorization", obj.token_type + " " + obj.access_token);
+
+            // execute the request
+            IRestResponse response = client.ExecuteAsPost(request, "POST");
+
+            var objEstadoRegistro = response.Content;
+
+
+        }
+    }
+}
---
+++ BegoSys.Service.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BegoSys.Service.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BegoSys.Service.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("88b7239e-ec74-435c-a7f4-3ac882c22aef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
