e3f0abd7cdb8de9974ed4628d66302f0de1d01bf
MauricioMC97 <36861255+MauricioMC97@users.noreply.github.com>
2022-01-17 22:13:35 -0500

    Add files via upload

---
+++ BegoSys.Service/Proveedores/ProveedorAutorizacionDirectorioActivoServidorBegoSys.cs
@@ -0,0 +1,77 @@
+﻿#region Derechos Reservados
+// ===================================================
+// Desarrollado Por             : Mauricio Medina
+// Fecha de Creación            : 2018/12/18
+// Modificado Por               : Mauricio Medina
+// Fecha Modificación           : 2018/12/18
+// Empresa                      : BEGO INVERSIONES S.A.S
+// ===================================================
+#endregion
+
+#region Referencias
+using BegoSys.Common.Auxiliares;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+#endregion
+
+namespace BegoSys.Service.Proveedores
+{
+    /// <summary>
+    /// Proveedor de autorización personalizado para el proyecto SGDHM
+    /// </summary>
+    public class ProveedorAutorizacionDirectorioActivoServidorSgdhm : OAuthAuthorizationServerProvider
+    {
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+            return Task.FromResult<object>(null);
+        }
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            string userName = context.UserName;
+            string password = context.Password;
+
+            //Get domain from the request
+            int domainIndex = userName.IndexOf(@"\");
+            string domain = string.Empty;
+            string defaultPassword = AuxiliarBegoSys.ObtenerAtributoDeConfiguracion("DefaultPasswordAPI", true);
+
+            if (domainIndex != -1)
+            {
+                domain = userName.Substring(0, domainIndex);
+                userName = userName.Substring(domainIndex + 1);
+            }
+            else
+            {
+                domain = AuxiliarBegoSys.ObtenerAtributoDeConfiguracion("DefaultDomainAPI", true);
+            }
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            using (PrincipalContext principalContext = new PrincipalContext(ContextType.Domain, domain))
+            {
+                UserPrincipal user = UserPrincipal.FindByIdentity(principalContext, userName);
+
+                if (user == null || !user.Enabled.Value || defaultPassword.ToUpper() != password.ToUpper())
+                {
+                    context.SetError("invalid_grant", "username or password invalid");
+                    return Task.FromResult<object>(null);
+                }
+
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim("sub", context.UserName));
+                identity.AddClaim(new Claim("role", "user"));
+
+                context.Validated(identity);
+            }
+
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ BegoSys.Service/Proveedores/ProveedorAutorizacionServidorBegoSys.cs
@@ -0,0 +1,58 @@
+﻿#region Derechos Reservados
+// ===================================================
+// Desarrollado Por             : MAURICIO MEDINA
+// Fecha de Creación            : 2018/12/18
+// Modificado Por               : MAURICIO MEDINA
+// Fecha Modificación           : 2018/12/18
+// Empresa                      : BEGO INVERSIONES  S.A.S
+// ===================================================
+#endregion
+
+#region Referencias
+using BegoSys.Common.Auxiliares;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+#endregion
+
+namespace BegoSys.Service.Proveedores
+{
+    /// <summary>
+    /// Proveedor de autorización personalizado para el proyecto SGDHM
+    /// </summary>
+    public class ProveedorAutorizacionServidorSgdhm : OAuthAuthorizationServerProvider
+    {
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+            return Task.FromResult<object>(null);
+        }
+
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+            //Se obtienen las credenciales por defecto para el consumo del servicio
+            string defaultUserName = AuxiliarBegoSys.ObtenerAtributoDeConfiguracion("DefaultUserNameAPI", true);
+            string defaultPassword = AuxiliarBegoSys.ObtenerAtributoDeConfiguracion("DefaultPasswordAPI", true);
+            bool isValid = (defaultUserName.ToUpper() == context.UserName?.ToUpper() && defaultPassword.ToUpper() == context.Password?.ToUpper());
+
+            if (!isValid)
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return Task.FromResult<object>(null);
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+            return Task.FromResult<object>(null);
+        }
+    }
+}
