d3417fc602bf1e45f2372d7031d0d3dba1e2b262
MauricioMC97 <36861255+MauricioMC97@users.noreply.github.com>
2022-01-18 15:14:18 -0500

    Add files via upload

---
+++ BegoSys.TO/PedidoTO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BegoSys.TO
+{
+    public class PedidoTO
+    {
+        //Clase que contiene los pedidos de los clientes y la información para que se puedan atender
+
+        public long idregistro { get; set; }
+
+        //Número del pedido en el día
+        public long IdPedidoDia { get; set; }
+
+        //Nombre del producto
+        public string NombreProd { get; set; }
+
+        //Fecha
+        public DateTime Fecha { get; set; }
+
+        //Cantidad del producto
+        public long Cantidad { get; set; }
+
+        //Estado Pedido
+        public string EstadoPedido { get; set; }
+
+        //Describe si es para consumir acá, llevar o domicilio
+        public string nombretipodespacho { get; set; }
+
+        /// <summary>
+        /// /////observaciones que hace el cliente al pedido
+        /// </summary>
+        public string observaciones { get; set; }
+
+        //Subtotal
+        public long subtotal { get; set; }
+    }
+}
---
+++ BegoSys.TO/PersonasLocalTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BegoSys.TO
+{
+    public class PersonasLocalTO
+    {
+        //Id Local
+        public long idLocalComercial { get; set; }
+
+        //Id Persona
+        public long idPersona { get; set; }
+
+        //Documento Persona
+        public string documento { get; set; }
+
+        //Nombre Completo
+        public string nombre { get; set; }
+
+        //Rol
+        public long idRol { get; set; }
+
+        //Salario por segundo de la persona usado para calculos de costos
+        public double SalarioSeg { get; set; }
+    }
+}
